c369ab772d723eb56172b32b14e9bf17f57b2167
Mayuge <mayuge.studio.public@gmail.com>
2024-06-02 21:21:59 +0900

    fix help message

--- main.c
+++ main.c
@@ -63,7 +63,11 @@ char *shift_arg(int *argc, char** *argv)
 
 void usage(const char *program_name)
 {
-    fprintf(stderr, "Usage: %s [-a | --add] | [-l | --list]\n", program_name);
+    printf("Usage: %s <subcommand> <args> <options>\n", program_name);
+    printf("        add          Add todo\n");
+    printf("        list         List todos\n");
+    printf("        complete     Mark todo as complete\n");
+    printf("        uncomplete   Mark todo as uncomplete\n");
 }
 
 enum Error load_todos(const char *filename, Todo_List *todo_list)
